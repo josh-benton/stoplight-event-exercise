658ba88616a88813f2d6080e6f37c9262574185f
josh-benton <joshuabenton@Joshuas-MacBook-Pro.local>
2023-03-03 15:50:55 -0500

    Enabled toggling of all traffic lights

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-(function() {
-  'use strict';
+const stopBtn = document.getElementById('stopButton');
+const slowBtn = document.getElementById('slowButton');
+const goBtn = document.getElementById('goButton');
+const stopLt = document.getElementById('stopLight');
+const slowLt = document.getElementById('slowLight');
+const goLt = document.getElementById('goLight');
 
-  // YOUR CODE HERE
-})();
+stopBtn.addEventListener('click', () => {
+    if (stopLt.classList.contains('stop')) {
+        stopLt.classList.remove('stop');
+    } else {
+        stopLt.classList.add('stop');
+    }
+})
+
+slowBtn.addEventListener('click', () => {
+    if (slowLt.classList.contains('slow')) {
+        slowLt.classList.remove('slow');
+    } else {
+        slowLt.classList.add('slow');
+    }
+})
+
+goBtn.addEventListener('click', () => {
+    if (goLt.classList.contains('go')) {
+        goLt.classList.remove('go');
+    } else {
+        goLt.classList.add('go');
+    }
+})
