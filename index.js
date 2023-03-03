c75e0ab0d28a84f752fff07e7e397e1e389e3b77
josh-benton <joshuabenton@Joshuas-MacBook-Pro.local>
2023-03-03 16:00:49 -0500

    Updated to log button text to console on mouseenter and mouseleave

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const goBtn = document.getElementById('goButton');
 const stopLt = document.getElementById('stopLight');
 const slowLt = document.getElementById('slowLight');
 const goLt = document.getElementById('goLight');
+let enterEventCount = 0;
+let leaveEventCount = 0;
+
 
 stopBtn.addEventListener('click', () => {
     if (stopLt.classList.contains('stop')) {
@@ -27,4 +30,28 @@ goBtn.addEventListener('click', () => {
     } else {
         goLt.classList.add('go');
     }
+})
+
+stopBtn.addEventListener('mouseenter', () => {
+    console.log(`Entered ${stopBtn.innerText} button`)
+})
+
+stopBtn.addEventListener('mouseleave', () => {
+    console.log(`Left ${stopBtn.innerText} button`)
+})
+
+slowBtn.addEventListener('mouseenter', () => {
+    console.log(`Entered ${slowBtn.innerText} button`)
+})
+
+slowBtn.addEventListener('mouseleave', () => {
+    console.log(`Left ${slowBtn.innerText} button`)
+})
+
+goBtn.addEventListener('mouseenter', () => {
+    console.log(`Entered ${goBtn.innerText} button`)
+})
+
+goBtn.addEventListener('mouseleave', () => {
+    console.log(`Left ${goBtn.innerText} button`)
 })
